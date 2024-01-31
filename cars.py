103405f16b9bf34f982b69246d991d9f8c84276a
Sara Nashbat <saranashbat@gmail.com>
2024-01-31 10:47:16 +0300

    local-2 first commit

--- cars.py
+++ cars.py
@@ -5,3 +5,9 @@ cars = ["Audi", "Toyota", "Kia", "Nissan", "Tesla"]
 randomNum = random.randint(0, len(cars)-1)
 
 print(cars[randomNum])
+
+def modelYear():
+	year = random.randint(2000, 2024)
+	return year
+
+print(modelYear())
